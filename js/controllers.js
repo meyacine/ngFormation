9081ac9c0d76912e21e61a19082a1aa0edc14717
meyacine <my.meddah@gmail.com>
2015-11-09 21:26:39 +0100

    validation ok

--- js/controllers.js
+++ js/controllers.js
@@ -6,22 +6,13 @@ app.controller('autreCtrl', function($scope){
     $scope.listeNom = [];
     $scope.message='';
     $scope.ajoutePersonne = function(nom){
-        if (nom==''){
-            $scope.message="Oops you can''t add a person without name :)";
-            $('#alerteDialog').modal('show');
+        if ($scope.listeNom.indexOf(nom) < 0) {
+            $scope.listeNom.push(nom);
+            //TODO : lancer un appel au serveur pour percister les données
+            nom='';
         } else {
-            if (!isNaN(nom)){
-                $scope.message="Your shure O.o have you seen a person called : "+nom+" !";
-                $('#alerteDialog').modal('show');
-            } else  {
-                if ($scope.listeNom.indexOf(nom) < 0) {
-                    $scope.listeNom.push(nom);
-                    nom='';
-                } else {
-                    $scope.message='Oops seems like this person is already added';
-                    $('#alerteDialog').modal('show');
-                }
-            }
+            $scope.message='Oops seems like this person is already added';
+            $('#alerteDialog').modal('show');
         }
     };
 });
