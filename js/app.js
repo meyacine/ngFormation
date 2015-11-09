f32fcb21c98ba3f62e0f1690236b09cd61da4990
meyacine <my.meddah@gmail.com>
2015-11-09 20:07:46 +0100

    databinding with routing ok$

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@
  * Created by meyacine on 09/11/2015.
  * Angular Application for formation
  */
-var app = angular.module('ngFormationApp',[]);
+var app = angular.module('ngFormationApp',['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'partials/home.html'
+    }).when('/autres', {
+        templateUrl: 'partials/other.html'
+    }).otherwise({
+        redirectTo: '/home'
+    });
+}]);
